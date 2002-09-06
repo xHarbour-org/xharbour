804b05db3edc0d1ec493a890af98878e1c65dd92
fsgiudice <fsgiudice@unknown.com>
2002-09-06 01:52:29 +0000

    Added

---
+++ xharbour/contrib/WoopGUI/make_b32.bat
@@ -0,0 +1,11 @@
+if not exist .\lib md .\lib
+if not exist .\obj md .\obj
+make -fmakefile.bc
+if errorlevel 1 goto error
+goto copy_files
+:error
+echo there is an error on make files
+go end
+:copy_files
+copy lib\*.lib ..\..\lib
+:end
