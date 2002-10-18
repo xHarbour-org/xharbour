08fcae555bfa27bcec289e003cda0499b26664d2
fsgiudice <fsgiudice@unknown.com>
2002-10-18 18:13:50 +0000

    Added "clean" option

--- xharbour/contrib/what32/make_b32.bat
+++ xharbour/contrib/what32/make_b32.bat
@@ -1,12 +1,26 @@
 @echo off
 if not exist .\lib md .\lib
 if not exist .\obj md .\obj
+
+if "%1" == "clean" goto CLEAN
+if "%1" == "CLEAN" goto CLEAN
+
 make -fmakefile.bc
 if errorlevel 1 goto error
 goto copy_files
+
+:clean
+
+   if exist lib\*.lib del .\lib\*.lib
+   if exist obj\*.obj del .\obj\*.obj
+   if exist obj\*.c   del .\obj\*.c
+   goto End
+
 :error
 echo there is an error on make files
 goto end
+
 :copy_files
 copy lib\*.lib ..\..\lib
+
 :end
