a90086113d7c5c7c91bccf05c0ea63ba25316a87
Paul Tucker <paultucker@unknown.com>
2002-11-03 15:17:44 +0000

    2002-11-03 10:12 UTC -0500 Paul Tucker <ptucker@sympatico.ca>

--- xharbour/contrib/what32/make_b32.bat
+++ xharbour/contrib/what32/make_b32.bat
@@ -1,6 +1,8 @@
 @echo off
 if not exist .\lib md .\lib
 if not exist .\obj md .\obj
+if not exist .\lib\b32 md .\lib\b32
+if not exist .\obj\vc md .\obj\vc
 
 if "%1" == "clean" goto CLEAN
 if "%1" == "CLEAN" goto CLEAN
@@ -11,9 +13,9 @@ goto copy_files
 
 :clean
 
-   if exist lib\*.lib del .\lib\*.lib
-   if exist obj\*.obj del .\obj\*.obj
-   if exist obj\*.c   del .\obj\*.c
+   if exist lib\b32\*.lib del .\lib\b32\*.lib
+   if exist obj\b32\*.obj del .\obj\b32\*.obj
+   if exist obj\b32\*.c   del .\obj\b32\*.c
    goto End
 
 :error
@@ -21,6 +23,7 @@ echo there is an error on make files
 goto end
 
 :copy_files
+copy lib\b32\*.lib lib
 copy lib\*.lib ..\..\lib
 
 :end
