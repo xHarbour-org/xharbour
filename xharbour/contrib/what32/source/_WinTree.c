fb1b887652f4596b23f94edd609a6a6b077363f7
Paul Tucker <paultucker@unknown.com>
2002-11-03 06:21:34 +0000

    *** empty log message ***

--- xharbour/contrib/what32/source/_WinTree.c
+++ xharbour/contrib/what32/source/_WinTree.c
@@ -13,10 +13,10 @@ HB_FUNC( TVINSERTITEM )
    is.hInsertAfter = TVI_LAST;
 
    #if (_WIN32_IE >= 0x0400)
-      is.DUMMYUNIONNAME.item.pszText = hb_parc( 2 );
-      is.DUMMYUNIONNAME.item.mask    = TVIF_TEXT | TVIF_IMAGE | TVIF_SELECTEDIMAGE;
-      is.DUMMYUNIONNAME.item.iImage  = hb_parnl( 4 );
-      is.DUMMYUNIONNAME.item.iSelectedImage = hb_parnl( 4 );
+      is.item.pszText = hb_parc( 2 );
+      is.item.mask    = TVIF_TEXT | TVIF_IMAGE | TVIF_SELECTEDIMAGE;
+      is.item.iImage  = hb_parnl( 4 );
+      is.item.iSelectedImage = hb_parnl( 4 );
    #else
       is.item.pszText = hb_parc( 2 );
       is.item.mask    = TVIF_TEXT | TVIF_IMAGE | TVIF_SELECTEDIMAGE;
@@ -54,7 +54,7 @@ HB_FUNC( TVGETSELTEXT ) // ( hWnd ) --> cText
    {
       tvi.mask       = TVIF_TEXT;
       tvi.hItem      = hItem;
-      tvi.pszText    = buffer;
+      tvi.pszText    = ( char *)buffer;
       tvi.cchTextMax = 100;
       TreeView_GetItem( hWnd, &tvi );
       hb_retc( tvi.pszText );
