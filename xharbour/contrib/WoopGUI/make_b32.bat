aeb13f6bd488e10159a254e1a199c43e898f69e6
Paul Tucker <paultucker@unknown.com>
2002-11-04 16:02:43 +0000

    See Changelog 2002-11-04 10:45 UTC -0500

--- xharbour/contrib/WoopGUI/make_b32.bat
+++ xharbour/contrib/WoopGUI/make_b32.bat
@@ -1,6 +1,8 @@
 @echo off
 if not exist .\lib md .\lib
 if not exist .\obj md .\obj
+if not exist .\lib\b32 md .\lib\b32
+if not exist .\obj\b32 md .\obj\b32
 make -fmakefile.bc
 if errorlevel 1 goto error
 goto copy_files
@@ -8,5 +10,6 @@ goto copy_files
 echo there is an error on make files
 goto end
 :copy_files
-copy lib\*.lib ..\..\lib
+copy lib\b32\*.lib lib
+copy lib\b32\*.lib ..\..\lib
 :end
