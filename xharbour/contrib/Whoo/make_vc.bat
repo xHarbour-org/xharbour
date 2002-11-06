13c23e148eb3cd82d4f5dfa4848d0102fcc02c33
Paul Tucker <paultucker@unknown.com>
2002-11-06 05:11:01 +0000

    See Changelo 2002-11-06 00:00 UTC-0500

--- xharbour/contrib/Whoo/make_vc.bat
+++ xharbour/contrib/Whoo/make_vc.bat
@@ -1,35 +1,63 @@
 @echo off
-rem $Id: make_vc.bat,v 1.2 2002/11/03 15:17:44 paultucker Exp $
-if not exist .\lib md .\lib
-if not exist .\lib\vc md .\lib\vc
-if not exist .\obj md .\obj
-if not exist .\obj\vc md .\obj\vc
+rem 
+rem $Id: make_vc.bat,v 1.3 2002/11/05 06:04:24 paultucker Exp $
+rem 
 
-if "%1" == "clean" goto CLEAN
-if "%1" == "CLEAN" goto CLEAN
+rem ---------------------------------------------------------------
+rem This is a generic template file, if it doesn't fit your own needs 
+rem please DON'T MODIFY IT.
+rem
+rem Instead, make a local copy and modify that one, or make a call to 
+rem this batch file from your customized one. [vszakats]
+rem ---------------------------------------------------------------
+
+set rem=rem
+if "%1"=="/?" set rem=echo.
+%rem% ---------------------------------------------------------------
+%rem% Usage: make_vc [/y] [/a or CLEAN or other specific target]
+%rem% Call with nothing, /Y, /A, or CLEAN
+%rem% nothing - compiles what needs it.
+%rem% CLEAN, delete targets.
+%rem% /A clean, then compile all
+%rem% /Y non batch mode (forces makefile.vc)
+%rem% ---------------------------------------------------------------
+set rem=
+if "%1"=="/?" goto exit
+
+if not exist obj md obj
+if not exist obj\vc md obj\vc
+if not exist lib md lib
+if not exist lib\vc md lib\vc
+
 set MK_FILE=makefile.vc
 if "%OS%" == "Windows_NT" set MK_FILE=makefile.nt
 if "%1" == "/Y" set MK_FILE=makefile.vc
 if "%1" == "/y" set MK_FILE=makefile.vc
+if "%1" == "clean" goto CLEAN
+if "%1" == "CLEAN" goto CLEAN
 
-nmake -f%MK_FILE% %1 %2 %3
-if errorlevel 1 goto error
-goto copy_files
+:BUILD
+
+   nmake /f %MK_FILE% %1 %2 %3
+   if errorlevel 1 goto error
+
+:BUILD_OK
+
+   copy lib\vc\*.lib lib >nul
+   copy lib\*.lib ..\..\lib >nul
+   goto exit
+
+:error
+
+   echo. there is an error building whoo
+   goto exit
 
 :clean
 
-   if exist lib\vc\*.lib del .\lib\vc\*.lib
-   if exist obj\vc\*.obj del .\obj\vc\*.obj
-   if exist obj\vc\*.c   del .\obj\vc\*.c
-   goto End
+   nmake /f %MK_FILE% %1
+   rem in this case, the makefile handles most cleanup. Add what you need here
+   rem if exist make_vc.log del make_vc.log
+   rem etc.
 
-:error
-echo there is an error on make files
-goto end
-
-:copy_files
-copy lib\vc\*.lib lib
-copy lib\*.lib ..\..\lib
-
-:end
-SET MK_FILE=
+:exit
+set MK_FILE=
