202280cb75de15d31311f427a48b906873a42405
fsgiudice <fsgiudice@unknown.com>
2003-01-11 02:18:09 +0000

    2003-01-11 03:15 UTC+0100 Francesco Saverio Giudice <info@fsgiudice.com>   * samples/sitesrv/bldsrv.bat     * fixed error on bld.bat path

--- xharbour/samples/sitesvr/bldsvr.bat
+++ xharbour/samples/sitesvr/bldsvr.bat
@@ -1,13 +1,13 @@
 @echo off
 rem
-rem $Id: bldsvr.bat,v 1.2 2003/01/11 00:25:17 ronpinkas Exp $
+rem $Id: bldsvr.bat,v 1.3 2003/01/11 01:53:48 ronpinkas Exp $
 rem
 
 rem ---------------------------------------------------------------
-rem This is a generic template file, if it doesn't fit your own needs 
+rem This is a generic template file, if it doesn't fit your own needs
 rem please DON'T MODIFY IT.
 rem
-rem Instead, make a local copy and modify that one, or make a call to 
+rem Instead, make a local copy and modify that one, or make a call to
 rem this batch file from your customized one. [vszakats]
 rem ---------------------------------------------------------------
 
@@ -19,4 +19,4 @@ set HB_LIB_INSTALL=..\..\lib
 
 SET HB_MT=mt
 
-call bld.bat sitesvr blddbf %1 %2 %3 %4 %5
+call %HB_BIN_INSTALL%\bld.bat sitesvr blddbf %1 %2 %3 %4 %5
