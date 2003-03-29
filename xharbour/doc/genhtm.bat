b7d4c3799e8c8515663f44155b81c7a9a57a6812
Patrick Mast <patrick@winfakt.be>
2003-03-29 11:01:11 +0000

    2003-03-29 11:58 UTC+0100 Patrick Mast <patrick@winfakt.com>   * doc/genhtm.bat     * minor fixes   + doc/en/zipnew.txt     * + Some modifications   * doc/genhtm.rsp     * Added all available documentation files from the /en map

--- xharbour/doc/genhtm.bat
+++ xharbour/doc/genhtm.bat
@@ -1,9 +1,11 @@
 @echo off
 rem 
-rem $Id: genhtm.bat,v 1.1.1.1 2001/12/21 10:48:09 ronpinkas Exp $
+rem $Id: genhtm.bat,v 1.2 2003/01/10 18:26:07 patrickmast Exp $
 rem 
 
+del htm\*.htm
 ..\bin\b32\hbdoc -htm genhtm.lnk genhtm.rsp
 cd htm
 echo renaming Harbour.htm to index.htm
 ren harbour.htm index.htm
+cd ..
