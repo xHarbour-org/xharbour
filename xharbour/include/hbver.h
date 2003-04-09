fe209345b59ef7445207977df9aa10719b555311
Luiz Rafael Culik <culik@sl.conex.net>
2003-04-09 15:59:31 +0000

    *** empty log message ***

--- xharbour/include/hbver.h
+++ xharbour/include/hbver.h
@@ -1,5 +1,5 @@
 /*
- * $Id: hbver.h,v 1.16 2003/04/08 18:32:21 lculik Exp $
+ * $Id: hbver.h,v 1.17 2003/04/08 18:38:48 lculik Exp $
  */
 
 /*
@@ -55,15 +55,15 @@
 
 #define HB_VER_LEX "SimpLex"
 #define HB_VER_MAJOR    0       /* Major version number */
-#define HB_VER_MINOR    82      /* Minor version number */
+#define HB_VER_MINOR    80      /* Minor version number */
 #define HB_VER_REVISION 0       /* Revision number */
 
 /* TOFIX: Ideally these should be generated dynamically, until
           then, they should be updated by the builder. [vszakats] */
 
 
-#define HB_VER_LENTRY   "2003-04-08 15:30 UTC-03000 Luiz Rafael Culik $"
-#define HB_VER_CHLCVS   "ChangeLog,v 1.927"
+#define HB_VER_LENTRY   "2003-04-09 13:00 UTC-0300 Luiz Rafael Culik"
+#define HB_VER_CHLCVS   "ChangeLog,v 1.932"
 #define HB_VER_C_USR    ""
 #define HB_VER_L_USR    ""
 #define HB_VER_PRG_USR  ""
