44a3b1fb769b71923c0645fd8045516c518584a4
Paul Tucker <paultucker@unknown.com>
2003-09-25 13:07:27 +0000

    See Changelog 2003-09-25 09:08 UTC-0500 Paul Tucker

--- xharbour/contrib/hbzlib/make_b32.bat
+++ xharbour/contrib/hbzlib/make_b32.bat
@@ -1,10 +1,8 @@
 @echo off
 rem
-rem $Id: make_b32.bat,v 1.4 2003/09/12 21:04:44 paultucker Exp $
+rem $Id: make_b32.bat,v 1.5 2003/09/23 04:52:34 paultucker Exp $
 rem
 
-if "%1" == "clean" goto CLEAN
-if "%1" == "CLEAN" goto CLEAN
 if not exist obj md obj
 if not exist obj\b32 md obj\b32
 
@@ -12,6 +10,8 @@ if not exist obj\b32 md obj\b32
 
    make -fmakefile.bc %1 %2 %3 > make_b32.log
    if errorlevel 1 goto BUILD_ERR
+   if "%1" == "clean" goto exit
+   if "%1" == "CLEAN" goto exit
 
 :BUILD_OK
    copy ..\..\lib\b32\hbzip.lib ..\..\lib > nul
@@ -20,10 +20,6 @@ if not exist obj\b32 md obj\b32
 :BUILD_ERR
 
    notepad make_b32.log
-   goto EXIT
-
-:CLEAN
-
    goto EXIT
 
 :EXIT
