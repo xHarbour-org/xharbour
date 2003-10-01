08c651cb5e2fa39d646e069f458d3225265a1df9
Ron Pinkas <ron@ronpinkas.com>
2003-10-01 19:22:33 +0000

    2003-10-01 12:16 UTC-0800 Ron Pinkas <ron@ronpinkas.com>    * dll_vc.bat    * hrbdll.vc    * tests/vcdll.bat      - Removed all settings and settings' dependancy.      + Added missing pplib modules.

--- xharbour/dll_vc.bat
+++ xharbour/dll_vc.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM
-REM $Id: dll_vc.bat,v 1.3 2003/05/28 03:06:29 paultucker Exp $
+REM $Id: dll_vc.bat,v 1.4 2003/09/25 02:02:31 paultucker Exp $
 REM
 REM 旼컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴커
 REM � This is a batch file to create harbour.dll 넴
@@ -12,7 +12,7 @@ if not exist obj md obj
 if not exist obj\dll md obj\dll
 if not exist obj\dll\vc md obj\dll\vc
 
-   nmake /f hrbdll.vc %1 %2 %3 >dll_vc.log
+   nmake /f hrbdll.vc %1 %2 %3 > dll_vc.log
    if errorlevel 1 goto BUILD_ERR
    if "%1" == "clean" goto exit
    if "%1" == "CLEAN" goto exit
@@ -20,10 +20,11 @@ if not exist obj\dll\vc md obj\dll\vc
    copy lib\vc\harbour.lib lib > nul
    copy lib\vc\harbour.exp lib > nul
    copy lib\vc\harbour.dll lib > nul
+   copy lib\vc\harbour.dll tests > nul
 
    goto EXIT
 
 :BUILD_ERR
-   notepad hrbdllvc.log
+   notepad dll_vc.log
 
 :EXIT
--- xharbour/tests/vcdll.bat
+++ xharbour/tests/vcdll.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM
-REM $Id: vcdll.bat,v 1.1 2002/01/06 03:45:43 andijahja Exp $
+REM $Id: vcdll.bat,v 1.1 2002/01/07 04:08:11 andijahja Exp $
 REM
 REM  ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿
 REM  ³ This is a template for building application with MsVC   ³Û
@@ -12,40 +12,20 @@ REM   ßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßß
 if "%1" == "" GOTO :SINTAX
 if NOT EXIST %1.prg GOTO :NOEXIST
 
-rem set envar for import library
-rem please adjust accordingly
-SET _HARBOURLIB_=c:\xhrb\lib\vc\harbour.lib
-
-rem set envar for Microsoft C++ Directory here
-rem please adjust accordingly
-SET __MSVCDIR__=C:\COMPILER\VC
-
-rem set envar for HARBOUR Directory here
-rem please adjust accordingly
-SET __HARBOURDIR__=C:\XHRB
-
-rem envar for Microsoft C++
-SET _LIB=%LIB%
-SET _PATH=%PATH%
-SET _INCLUDE=%INCLUDE%
-SET LIB=%__MSVCDIR__%\LIB;%__HARBOURDIR__%\LIB\VC;%PATH%
-SET PATH=%__MSVCDIR__%\BIN;%__HARBOURDIR__%\BIN\VC;%PATH%
-SET INCLUDE=%__HARBOURDIR__%\INCLUDE;%__MSVCDIR__%\INCLUDE;%_INCLUDE%
-
 ECHO Compiling...
 
-harbour %1 /n %2 %3
+harbour %1 -n -I..\include %2 %3
 
 IF ERRORLEVEL 1 PAUSE
 IF ERRORLEVEL 1 GOTO EXIT
 
 rem using MT flag !!
-cl /Ox /G6 /MT /TP /W3 /c /GA %1.c
-cl /Ox /G6 /MT /TP /W3 /c /GA %__HARBOURDIR__%\source\vm\mainstd.c
+cl -I..\include /Ox /G6 /MT /TP /W3 /c /GA %1.c
+cl -I..\include /Ox /G6 /MT /TP /W3 /c /GA ..\source\vm\mainstd.c
 
 echo %1.obj  > msvc.tmp
 echo mainstd.obj  >> msvc.tmp
-echo %_HARBOURLIB_% >> msvc.tmp
+echo ..\lib\vc\harbour.lib >> msvc.tmp
 echo winspool.lib >> msvc.tmp
 echo user32.lib >> msvc.tmp
 link @msvc.tmp /nodefaultlib:libc.lib /out:%1.exe /nologo /subsystem:console
@@ -53,16 +33,6 @@ link @msvc.tmp /nodefaultlib:libc.lib /out:%1.exe /nologo /subsystem:console
 rem delete temporary files
 @del %1.c
 @del msvc.tmp
-
-SET LIB=%_LIB%
-SET PATH=%_PATH%
-SET INCLUDE=%_INCLUDE%
-SET _LIB=
-SET _PATH=
-SET _INCLUDE=
-SET __HARBOURDIR__=
-SET __MSVCDIR__=
-SET _HARBOURLIB_=
 
 IF ERRORLEVEL 1 GOTO LINKERROR
 IF EXIST %1.exe dir %1.exe
