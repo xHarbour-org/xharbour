0ad9587b087e7f43c4e8d9e3af7cb93d06a56f2a
Andi Jahja <andi.jahja@yahoo.co.id>
2004-01-12 13:58:21 +0000

    andijahja

--- xharbour/include/hbgtwvt.h
+++ xharbour/include/hbgtwvt.h
@@ -1,3 +1,55 @@
+/*
+ * $Id:
+ */
+
+/*
+ * Harbour Project source code:
+ * Header File for Video subsystem for Win32 using GUI windows instead of Console
+ * Copyright 2003 Peter Rees <peter@rees.co.nz>
+ *                Rees Software & Systems Ltd
+ * www - http://www.harbour-project.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA (or visit the web site http://www.gnu.org/).
+ *
+ * As a special exception, the Harbour Project gives permission for
+ * additional uses of the text contained in its release of Harbour.
+ *
+ * The exception is that, if you link the Harbour libraries with other
+ * files to produce an executable, this does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * Your use of that executable is in no way restricted on account of
+ * linking the Harbour library code into it.
+ *
+ * This exception does not however invalidate any other reasons why
+ * the executable file might be covered by the GNU General Public License.
+ *
+ * This exception applies only to the code released by the Harbour
+ * Project under the name Harbour.  If you copy code from other
+ * Harbour Project or Free Software Foundation releases into a copy of
+ * Harbour, as the General Public License permits, the exception does
+ * not apply to the code that you add in this way.  To avoid misleading
+ * anyone as to the status of such modified files, you must delete
+ * this exception notice from them.
+ *
+ * If you write modifications of your own for Harbour, it is your choice
+ * whether to permit this exception to apply to your modifications.
+ * If you do not wish that, delete this exception notice.
+ *
+ */
+
 #ifndef HB_WVT_H_
 #define HB_WVT_H_
 
@@ -36,16 +88,13 @@
 
    #if defined(_MSC_VER)
       #include <olectl.h>
+      #include <conio.h>
    #endif
 #else
    #include <olectl.h>
 #endif
 #include <time.h>
 #include <ctype.h>
-
-#if defined( _MSC_VER )
-  #include <conio.h>
-#endif
 
 //-------------------------------------------------------------------//
 
