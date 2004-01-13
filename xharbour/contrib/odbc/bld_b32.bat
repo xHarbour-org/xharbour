82d8cd56e4011fa03799653c8c7ed17bf95c5a63
Ron Pinkas <ron@ronpinkas.com>
2004-01-13 04:49:21 +0000

    Removed redundant files, see source\odbc

--- xharbour/contrib/odbc/bld_b32.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem
-rem $Id: bld_b32.bat,v 1.1 2004/01/13 01:16:13 ronpinkas Exp $
-rem
-
-..\..\bin\harbour %1 /n /i..\include %2
-bcc32 -e%1.exe -O2 -d -I..\..\include -L..\..\lib\b32 %1.c debug.lib vm.lib rtl.lib gtwin.lib lang.lib rdd.lib macro.lib pp.lib dbfntx.lib dbfdbt.lib dbfcdx.lib dbffpt.lib common.lib hbodbc.lib odbc32.lib
-
-rem del %1.c
--- xharbour/contrib/odbc/make_b32.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-make -fhbodbc.b32
-implib ..\..\lib\odbc32.lib odbc32.def
