47ebeb008100745ca0aec0d5260f36a8c4e9114a
guerra000 <guerra000@unknown.com>
2005-03-31 06:39:19 +0000

    2005-03-31 00:08 UTC-0600 Vicente Guerra <vicente@guerra.com.mx>   * ./hrbdll.bc   * ./hrbdll.dc   * ./hrbdll.gc   * ./hrbdll.pc   * ./hrbdll.vc   * ./hrbdll.wc   * ./makefile.bc   * ./makefile.dc   * ./makefile.gc   * ./makefile.nt   * ./makefile.pc   * ./makefile.vc   * ./makefile.wc   * source/rtl/Makefile   * source/rtl/set.c   + source/rtl/readexit.c   + source/rtl/readins.c     * Moved ReadExit() and ReadInsert() functions from set.c to       separate files.

---
+++ xharbour/source/rtl/readins.c
@@ -0,0 +1,67 @@
+/*
+ * $Id: readins.c,v 1.0 2005/02/12 04:37:16 guerra000 Exp $
+ */
+
+/*
+ * xHarbour Project source code:
+ * ReadInsert() function
+ *
+ * Copyright 2005 Vicente Guerra <vicente@guerra.com.mx>
+ * www - http://www.xharbour.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA (or visit the web site http://www.gnu.org/).
+ *
+ * As a special exception, the xHarbour Project gives permission for
+ * additional uses of the text contained in its release of xHarbour.
+ *
+ * The exception is that, if you link the xHarbour libraries with other
+ * files to produce an executable, this does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * Your use of that executable is in no way restricted on account of
+ * linking the xHarbour library code into it.
+ *
+ * This exception does not however invalidate any other reasons why
+ * the executable file might be covered by the GNU General Public License.
+ *
+ * This exception applies only to the code released by the xHarbour
+ * Project under the name xHarbour.  If you copy code from other
+ * xHarbour Project or Free Software Foundation releases into a copy of
+ * xHarbour, as the General Public License permits, the exception does
+ * not apply to the code that you add in this way.  To avoid misleading
+ * anyone as to the status of such modified files, you must delete
+ * this exception notice from them.
+ *
+ * If you write modifications of your own for xHarbour, it is your choice
+ * whether to permit this exception to apply to your modifications.
+ * If you do not wish that, delete this exception notice.
+ *
+ */
+
+#include "hbapi.h"
+#include "hbapiitm.h"
+#include "hbfast.h"
+#include "hbstack.h"
+#include "hbset.h"
+
+HB_FUNC( READINSERT )
+{
+   hb_retl( hb_set.HB_SET_INSERT );
+
+   if( ISLOG( 1 ) )
+   {
+      hb_set.HB_SET_INSERT = hb_parl( 1 );
+   }
+}
