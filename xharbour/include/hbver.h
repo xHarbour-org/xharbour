dfa5c342fe8cbfd729b32873dde70ed6670055b4
Ron Pinkas <ron@ronpinkas.com>
2005-06-02 02:20:52 +0000

    Refreshed with 2005-06-02 03:46 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)

--- xharbour/include/hbver.h
+++ xharbour/include/hbver.h
@@ -1,5 +1,5 @@
 /*
- * $Id: hbver.h,v 1.63 2005/06/02 01:14:10 druzus Exp $
+ * $Id: hbver.h,v 1.64 2005/06/02 01:47:25 druzus Exp $
  */
 
 /*
@@ -62,7 +62,7 @@
           then, they should be updated by the builder. [vszakats] */
 
 
-#define HB_VER_LENTRY   "2005-06-02 03:12 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)"
+#define HB_VER_LENTRY   "2005-06-02 03:46 UTC+0200 Przemyslaw Czerpak (druzus/at/priv.onet.pl)"
 #define HB_VER_CHLCVS   "ChangeLog,v 1.4165"
 #define HB_VER_C_USR    ""
 #define HB_VER_L_USR    ""
