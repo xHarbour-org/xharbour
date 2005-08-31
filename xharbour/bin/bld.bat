64fa6d8f036a0013fbbdb7d1d3125c78bf8979a2
Maurilio Longo <maurilio.longo@libero.it>
2005-08-31 15:00:14 +0000

    2005-08-31 17:10 UTC+0200 Maurilio Longo <maurilio.longo@libero.it>    * makefile.bc      * changed to work with new pcre 6.3 regex engine    * bin\\bld.bat      * changed to add pcrepos.lib to built .exe files
       /* NOTE: This win32 build seems to work fine, I don't have other
                win32 compilers available, though.
       */

--- xharbour/bin/bld.bat
+++ xharbour/bin/bld.bat
@@ -1,6 +1,6 @@
 @echo off
 rem
-rem $Id: bld.bat,v 1.43 2004/08/02 01:46:12 maurifull Exp $
+rem $Id: bld.bat,v 1.50 2005/06/16 14:14:27 lf_sfnet Exp $
 rem
 
 rem ---------------------------------------------------------------
@@ -188,7 +188,7 @@ if "%HB_INC_INSTALL%" == "" set HB_INC_INSTALL=..\include
 :A_DOS_RSX32_NOT
 
    if not "%HB_COMPILER%" == "watcom" goto END
-      
+
       wpp386 -j -w2 -d1 -zq -bt=DOS -5 -fp5 -onaehtzr -oi+ -ei -zp8 -s -zt0 %1.c -fo=%1.obj
       echo debug all OP osn=DOS OP stack=65536 OP CASEEXACT OP stub=cwstub.exe NAME %1.exe > build.tmp
       echo FILE %1.obj >> build.tmp
@@ -221,23 +221,23 @@ if "%HB_INC_INSTALL%" == "" set HB_INC_INSTALL=..\include
 
    if "%HB_GTALLEG%" == "yes" set HB_ALGLIB=alleg.lib
 
-   if "%HB_DLL%" == "" set HB_LIBLIST=common.lib debug.lib vm%HB_MT%.lib rtl%HB_MT%.lib %_HB_GT_LIB%.lib lang.lib rdd%HB_MT%.lib macro%HB_MT%.lib pp%HB_MT%.lib dbfdbt%HB_MT%.lib dbffpt%HB_MT%.lib dbfntx%HB_MT%.lib dbfcdx%HB_MT%.lib samples.lib ct%HB_MT%.lib tip%HB_MT%.lib %ADS_LIBS% %HB_USER_LIBS% %HB_ALGLIB%
-   if not "%HB_DLL%" == "" set HB_LIBLIST=harbour.lib %_HB_GT_LIB%.lib samples.lib vm.lib %ADS_LIBS% %HB_USER_LIBS% %HB_ALGLIB%
+   if "%HB_DLL%" == "" set HB_LIBLIST=common.lib debug.lib vm%HB_MT%.lib rtl%HB_MT%.lib pcrepos.lib %_HB_GT_LIB%.lib lang.lib rdd%HB_MT%.lib macro%HB_MT%.lib pp%HB_MT%.lib dbfdbt%HB_MT%.lib dbffpt%HB_MT%.lib dbfntx%HB_MT%.lib dbfcdx%HB_MT%.lib samples.lib ct%HB_MT%.lib tip%HB_MT%.lib %ADS_LIBS% %HB_USER_LIBS% %HB_ALGLIB%
+   if not "%HB_DLL%" == "" set HB_LIBLIST=harbour.lib pcrepos.lib %_HB_GT_LIB%.lib samples.lib vm.lib %ADS_LIBS% %HB_USER_LIBS% %HB_ALGLIB%
 
    if exist %HB_LIB_INSTALL%\hbzip.lib set HB_LIBLIST=%HB_LIBLIST% hbzip.lib
-   
+
    if not "%HB_MT%" == "" SET BC_MT_FLAG=-tWM
    if "%HB_MT%" == "" SET BC_MT_FLAG=
 
    if "%HB_COMPILER%" == "bcc32"   if     exist ..\lib\bcc640%HB_MT%.lib bcc32 %BC_MT_FLAG% -O2 -d %CFLAGS% -I%HB_INC_INSTALL% -L%HB_LIB_INSTALL% %1.c %HB_2nd_prg% %HB_3rd_prg% bcc640%HB_MT%.lib %HB_LIBLIST%
    if "%HB_COMPILER%" == "bcc32"   if not exist ..\lib\bcc640.lib bcc32 %BC_MT_FLAG% -O2 -d %CFLAGS% -I%HB_INC_INSTALL% -L%HB_LIB_INSTALL% %1.c %HB_2nd_prg% %HB_3rd_prg% %HB_LIBLIST%
    if "%HB_COMPILER%" == "gcc"     gcc %1.c -o%1.exe %CFLAGS% -I%HB_INC_INSTALL% -L%HB_LIB_INSTALL% -ldebug -lvm -lrtl -l%_HB_GT_LIB% -llang -lrdd -lrtl -lvm -lmacro -lpp -ldbfdbt -ldbffpt -ldbfntx -ldbfcdx -lcommon -lct -ltip
- 
-  
+
+
    if "%HB_COMPILER%" == "rsxnt"   gcc %1.c -Zwin32 %CFLAGS% -I%HB_INC_INSTALL% -L%HB_LIB_INSTALL% -ldebug -lvm -lrtl -l%_HB_GT_LIB% -llang -lrdd -lrtl -lvm -lmacro -lpp -ldbfdbt -ldbffpt -ldbfntx -ldbfcdx -lcommon
    goto end
-   
-:C_MINGW32   
+
+:C_MINGW32
    if not "%HB_COMPILER%" == "mingw32" goto C_MSVC
    if "%HB_MULTI_GT%" == "yes" set _HB_GT_LIB=gtnul -l%_HB_GT_LIB%
    gcc %1.c -o%1.exe %HB_TMP_OBJ% %CFLAGS% -mno-cygwin %HB_TMP_INCLUDE% -I%HB_INC_INSTALL% -L%HB_LIB_INSTALL% %HB_TMP_INSTALL% -Wl,--start-group -ldebug -lvm%HB_MT% -lrtl%HB_MT% -l%_HB_GT_LIB% -llang -lrdd%HB_MT% -lrtl%HB_MT% -lvm%HB_MT% -lmacro -lpp%HB_MT% -ldbfdbt%HB_MT% -ldbffpt%HB_MT% -ldbfntx%HB_MT% -ldbfcdx%HB_MT% -lcodepage -lcommon -lct%HB_MT% -lhbodbc -ltip%HB_MT% %HB_TMP_LIB% -Wl,--end-group -luser32 -lwinspool -lole32 -loleaut32 -luuid -lgdi32 -lcomctl32 -lcomdlg32 -lodbc32 -lmapi32 -lws2_32
@@ -254,7 +254,7 @@ if "%HB_INC_INSTALL%" == "" set HB_INC_INSTALL=..\include
    if "%HB_MT%" == "" set LDFLAGS=/NODEFAULTLIB:LIBCMT
    if not "%HB_MT%" == "" set LDFLAGS=/NODEFAULTLIB:LIBC /NODEFAULTLIB:LIBCP
 
-   if not "%HB_DLL%" == "" if "%HB_MT%" == "" set LDFLAGS=%LDFLAGS% /NODEFAULTLIB:LIBC 
+   if not "%HB_DLL%" == "" if "%HB_MT%" == "" set LDFLAGS=%LDFLAGS% /NODEFAULTLIB:LIBC
 
    set _cons=CONSOLE
    set _main=std
@@ -292,7 +292,7 @@ if "%HB_INC_INSTALL%" == "" set HB_INC_INSTALL=..\include
    echo LIB common.lib >> build.tmp
    echo LIB hbodbc.lib >> build.tmp
    echo LIB ct%HB_MT%.lib >> build.tmp
-   echo LIB tip%HB_MT%.lib >> build.tmp 
+   echo LIB tip%HB_MT%.lib >> build.tmp
    echo LIB kernel32.lib >> build.tmp
    echo LIB user32.lib >> build.tmp
    echo LIB winspool.lib >> build.tmp
