f0e68a62595ac8989142029fb8cea457a4d760f9
modalsist <modalsist@unknown.com>
2005-11-17 00:51:06 +0000

    *** empty log message ***

--- xharbour/make_pc.bat
+++ xharbour/make_pc.bat
@@ -1,24 +1,21 @@
 @ECHO OFF
-rem $Id: make_pc.bat,v 1.4 2005/11/02 18:00:13 modalsist Exp $
+rem $Id: make_pc.bat,v 1.6 2005/11/16 00:00:00 modalsist Exp $
 rem
-rem Batch file for building xHarbour under MS-Windows with Pelles C compiler.
+rem Batch file Pelles C compiler.
 rem
-rem NOTE: Pelles C is a free C/C++ compliler for MS-Windows platform, for more
-rem       info visit: http://www.smorgasbordet.com/pellesc/index.htm
+rem NOTE: Pelles C is a free C/C++ compliler for MS-Windows platform.
+rem       http://www.smorgasbordet.com/pellesc/index.htm
 rem
 rem 
 
-REM *************************************
-REM *** CHANGE ONLY THE 3 SETS BELLOW ***
-REM *************************************
+REM ******************************************
+REM *** If necessary, change only 3 sets below
+REM ******************************************
 
 SET HB_DIR=.\
 SET POCC_DIR=c:\pellesc
 SET BISON_DIR=c:\bison
 
-REM *****************************************
-REM *** DON'T CHANGE THE VARIABLES BELLOW ***
-REM *****************************************
 
 SET _PATH=%PATH%
 SET _LIB=%LIB%
@@ -29,9 +26,9 @@ SET INCLUDE=%POCC_DIR\include;%POCC_DIR\include\win;%HB_DIR%\include;%INCLUDE%
 
 SET PATH=%POCC_DIR%\bin;%BISON_DIR%\bin;%HB_DIR%\bin;%PATH%
 
-SET TARGET_OBJ_DIR=%HB_DIR%\obj\pocc
-SET TARGET_LIB_DIR=%HB_DIR%\lib\pocc
-SET TARGET_BIN_DIR=%HB_DIR%\bin\pocc
+SET OBJ_DIR=%HB_DIR%\obj\pocc
+SET LIB_DIR=%HB_DIR%\lib\pocc
+SET BIN_DIR=%HB_DIR%\bin\pocc
 
 SET BISON_SIMPLE=%BISON_DIR%\share\bison\bison.simple
 
@@ -40,21 +37,21 @@ if "%1" == "clean" goto CLEAN
 if "%1" == "CLEAN" goto CLEAN
 
 
-  if not exist obj                              md obj
-  if not exist %TARGET_LIB_DIR%                 md %TARGET_LIB_DIR%
-  if not exist %TARGET_BIN_DIR%                 md %TARGET_BIN_DIR%
-  if not exist %TARGET_OBJ_DIR%                 md %TARGET_OBJ_DIR%
+  if not exist obj                       md obj
+  if not exist %LIB_DIR%                 md %LIB_DIR%
+  if not exist %BIN_DIR%                 md %BIN_DIR%
+  if not exist %OBJ_DIR%                 md %OBJ_DIR%
 
-  if not exist %TARGET_OBJ_DIR%\ct              md %TARGET_OBJ_DIR%\ct
-  if not exist %TARGET_OBJ_DIR%\opt             md %TARGET_OBJ_DIR%\opt
-  if not exist %TARGET_OBJ_DIR%\opt\console     md %TARGET_OBJ_DIR%\opt\console
-  if not exist %TARGET_OBJ_DIR%\opt\gui         md %TARGET_OBJ_DIR%\opt\gui
+  if not exist %OBJ_DIR%\ct              md %OBJ_DIR%\ct
+  if not exist %OBJ_DIR%\opt             md %OBJ_DIR%\opt
+  if not exist %OBJ_DIR%\opt\console     md %OBJ_DIR%\opt\console
+  if not exist %OBJ_DIR%\opt\gui         md %OBJ_DIR%\opt\gui
 
-  if not exist %TARGET_OBJ_DIR%\mt              md %TARGET_OBJ_DIR%\mt
-  if not exist %TARGET_OBJ_DIR%\mt\ct           md %TARGET_OBJ_DIR%\mt\ct
-  if not exist %TARGET_OBJ_DIR%\mt\opt          md %TARGET_OBJ_DIR%\mt\opt
-  if not exist %TARGET_OBJ_DIR%\mt\opt\console  md %TARGET_OBJ_DIR%\mt\opt\console
-  if not exist %TARGET_OBJ_DIR%\mt\opt\gui      md %TARGET_OBJ_DIR%\mt\opt\gui
+  if not exist %OBJ_DIR%\mt              md %OBJ_DIR%\mt
+  if not exist %OBJ_DIR%\mt\ct           md %OBJ_DIR%\mt\ct
+  if not exist %OBJ_DIR%\mt\opt          md %OBJ_DIR%\mt\opt
+  if not exist %OBJ_DIR%\mt\opt\console  md %OBJ_DIR%\mt\opt\console
+  if not exist %OBJ_DIR%\mt\opt\gui      md %OBJ_DIR%\mt\opt\gui
 
 
   echo Compiling binaries and core libs
@@ -142,116 +139,116 @@ if "%1" == "CLEAN" goto CLEAN
 
    REM Cleaning temp bin folder
 
-   if exist %TARGET_BIN_DIR%\harbour.exe          del %TARGET_BIN_DIR%\harbour.exe
-   if exist %TARGET_BIN_DIR%\harbour.exp          del %TARGET_BIN_DIR%\harbour.exp
+   if exist %BIN_DIR%\harbour.exe          del %BIN_DIR%\harbour.exe
+   if exist %BIN_DIR%\harbour.exp          del %BIN_DIR%\harbour.exp
 
-   if exist %TARGET_BIN_DIR%\xharbour.exp         del %TARGET_BIN_DIR%\xharbour.exp
-   if exist %TARGET_BIN_DIR%\xharbour.exe         del %TARGET_BIN_DIR%\xharbour.exe
-   if exist %TARGET_BIN_DIR%\xharbour.dll         del %TARGET_BIN_DIR%\xharbour.dll
+   if exist %BIN_DIR%\xharbour.exp         del %BIN_DIR%\xharbour.exp
+   if exist %BIN_DIR%\xharbour.exe         del %BIN_DIR%\xharbour.exe
+   if exist %BIN_DIR%\xharbour.dll         del %BIN_DIR%\xharbour.dll
 
-   if exist %TARGET_BIN_DIR%\hbdoc.exe            del %TARGET_BIN_DIR%\hbdoc.exe
-   if exist %TARGET_BIN_DIR%\hbdocdll.exe         del %TARGET_BIN_DIR%\hbdocdll.exe
+   if exist %BIN_DIR%\hbdoc.exe            del %BIN_DIR%\hbdoc.exe
+   if exist %BIN_DIR%\hbdocdll.exe         del %BIN_DIR%\hbdocdll.exe
 
-   if exist %TARGET_BIN_DIR%\hbmake.exe           del %TARGET_BIN_DIR%\hbmake.exe
-   if exist %TARGET_BIN_DIR%\hbmakedll.exe        del %TARGET_BIN_DIR%\hbmakedll.exe
+   if exist %BIN_DIR%\hbmake.exe           del %BIN_DIR%\hbmake.exe
+   if exist %BIN_DIR%\hbmakedll.exe        del %BIN_DIR%\hbmakedll.exe
 
-   if exist %TARGET_BIN_DIR%\hbpp.exe             del %TARGET_BIN_DIR%\hbpp.exe
+   if exist %BIN_DIR%\hbpp.exe             del %BIN_DIR%\hbpp.exe
 
-   if exist %TARGET_BIN_DIR%\hbrun.exe            del %TARGET_BIN_DIR%\hbrun.exe
-   if exist %TARGET_BIN_DIR%\hbrundll.exe         del %TARGET_BIN_DIR%\hbrundll.exe
-   if exist %TARGET_BIN_DIR%\hbrunmt.exe          del %TARGET_BIN_DIR%\hbrunmt.exe
+   if exist %BIN_DIR%\hbrun.exe            del %BIN_DIR%\hbrun.exe
+   if exist %BIN_DIR%\hbrundll.exe         del %BIN_DIR%\hbrundll.exe
+   if exist %BIN_DIR%\hbrunmt.exe          del %BIN_DIR%\hbrunmt.exe
 
-   if exist %TARGET_BIN_DIR%\hbtest.exe           del %TARGET_BIN_DIR%\hbtest.exe
-   if exist %TARGET_BIN_DIR%\hbtestdll.exe        del %TARGET_BIN_DIR%\hbtestdll.exe
-   if exist %TARGET_BIN_DIR%\hbtestmt.exe         del %TARGET_BIN_DIR%\hbtestmt.exe
+   if exist %BIN_DIR%\hbtest.exe           del %BIN_DIR%\hbtest.exe
+   if exist %BIN_DIR%\hbtestdll.exe        del %BIN_DIR%\hbtestdll.exe
+   if exist %BIN_DIR%\hbtestmt.exe         del %BIN_DIR%\hbtestmt.exe
 
-   if exist %TARGET_BIN_DIR%\xbscript.exe         del %TARGET_BIN_DIR%\xbscript.exe
-   if exist %TARGET_BIN_DIR%\xbscriptdll.exe      del %TARGET_BIN_DIR%\xbscriptdll.exe
+   if exist %BIN_DIR%\xbscript.exe         del %BIN_DIR%\xbscript.exe
+   if exist %BIN_DIR%\xbscriptdll.exe      del %BIN_DIR%\xbscriptdll.exe
 
    REM Cleaning temp lib folder
 
-   if exist %TARGET_LIB_DIR%\codepage.lib         del %TARGET_LIB_DIR%\codepage.lib
-   if exist %TARGET_LIB_DIR%\common.lib           del %TARGET_LIB_DIR%\common.lib
-   if exist %TARGET_LIB_DIR%\ct.lib               del %TARGET_LIB_DIR%\ct.lib
-   if exist %TARGET_LIB_DIR%\ctmt.lib             del %TARGET_LIB_DIR%\ctmt.lib
-   if exist %TARGET_LIB_DIR%\dbfcdx.lib           del %TARGET_LIB_DIR%\dbfcdx.lib
-   if exist %TARGET_LIB_DIR%\dbfcdxmt.lib         del %TARGET_LIB_DIR%\dbfcdxmt.lib
-   if exist %TARGET_LIB_DIR%\dbfdbt.lib           del %TARGET_LIB_DIR%\dbfdbt.lib
-   if exist %TARGET_LIB_DIR%\dbfdbtmt.lib         del %TARGET_LIB_DIR%\dbfdbtmt.lib
-   if exist %TARGET_LIB_DIR%\dbffpt.lib           del %TARGET_LIB_DIR%\dbffpt.lib
-   if exist %TARGET_LIB_DIR%\dbffptmt.lib         del %TARGET_LIB_DIR%\dbffptmt.lib
-   if exist %TARGET_LIB_DIR%\dbfntx.lib           del %TARGET_LIB_DIR%\dbfntx.lib
-   if exist %TARGET_LIB_DIR%\dbfntxmt.lib         del %TARGET_LIB_DIR%\dbfntxmt.lib
-   if exist %TARGET_LIB_DIR%\debug.lib            del %TARGET_LIB_DIR%\debug.lib
-   if exist %TARGET_LIB_DIR%\dllmain.lib          del %TARGET_LIB_DIR%\dllmain.lib
-   if exist %TARGET_LIB_DIR%\harbour.lib          del %TARGET_LIB_DIR%\harbour.lib
-   if exist %TARGET_LIB_DIR%\hbodbc.lib           del %TARGET_LIB_DIR%\hbodbc.lib
-   if exist %TARGET_LIB_DIR%\hbodbcmt.lib         del %TARGET_LIB_DIR%\hbodbcmt.lib
-   if exist %TARGET_LIB_DIR%\hbsix.lib            del %TARGET_LIB_DIR%\hbsix.lib
-   if exist %TARGET_LIB_DIR%\hbsixmt.lib          del %TARGET_LIB_DIR%\hbsixmt.lib
-   if exist %TARGET_LIB_DIR%\hsx.lib              del %TARGET_LIB_DIR%\hsx.lib
-   if exist %TARGET_LIB_DIR%\hsxmt.lib            del %TARGET_LIB_DIR%\hsxmt.lib
-   if exist %TARGET_LIB_DIR%\gtnul.lib            del %TARGET_LIB_DIR%\gtnul.lib
-   if exist %TARGET_LIB_DIR%\gtstd.lib            del %TARGET_LIB_DIR%\gtstd.lib
-   if exist %TARGET_LIB_DIR%\gtwin.lib            del %TARGET_LIB_DIR%\gtwin.lib
-   if exist %TARGET_LIB_DIR%\gtcgi.lib            del %TARGET_LIB_DIR%\gtcgi.lib
-   if exist %TARGET_LIB_DIR%\gtpca.lib            del %TARGET_LIB_DIR%\gtpca.lib
-   if exist %TARGET_LIB_DIR%\gtwvt.lib            del %TARGET_LIB_DIR%\gtwvt.lib
-   if exist %TARGET_LIB_DIR%\lang.lib             del %TARGET_LIB_DIR%\lang.lib
-   if exist %TARGET_LIB_DIR%\macro.lib            del %TARGET_LIB_DIR%\macro.lib
-   if exist %TARGET_LIB_DIR%\macromt.lib          del %TARGET_LIB_DIR%\macromt.lib
-   if exist %TARGET_LIB_DIR%\nulsys.lib           del %TARGET_LIB_DIR%\nulsys.lib
-   if exist %TARGET_LIB_DIR%\optcon.lib           del %TARGET_LIB_DIR%\optcon.lib
-   if exist %TARGET_LIB_DIR%\optconmt.lib         del %TARGET_LIB_DIR%\optconmt.lib
-   if exist %TARGET_LIB_DIR%\optgui.lib           del %TARGET_LIB_DIR%\optgui.lib
-   if exist %TARGET_LIB_DIR%\optguimt.lib         del %TARGET_LIB_DIR%\optguimt.lib
-   if exist %TARGET_LIB_DIR%\pcrepos.lib          del %TARGET_LIB_DIR%\pcrepos.lib
-   if exist %TARGET_LIB_DIR%\pcreposmt.lib        del %TARGET_LIB_DIR%\pcreposmt.lib
-   if exist %TARGET_LIB_DIR%\pp.lib               del %TARGET_LIB_DIR%\pp.lib
-   if exist %TARGET_LIB_DIR%\ppmt.lib             del %TARGET_LIB_DIR%\ppmt.lib
-   if exist %TARGET_LIB_DIR%\rdd.lib              del %TARGET_LIB_DIR%\rdd.lib
-   if exist %TARGET_LIB_DIR%\rddmt.lib            del %TARGET_LIB_DIR%\rddmt.lib
-   if exist %TARGET_LIB_DIR%\rtl.lib              del %TARGET_LIB_DIR%\rtl.lib
-   if exist %TARGET_LIB_DIR%\rtlmt.lib            del %TARGET_LIB_DIR%\rtlmt.lib
-   if exist %TARGET_LIB_DIR%\tip.lib              del %TARGET_LIB_DIR%\tip.lib
-   if exist %TARGET_LIB_DIR%\tipmt.lib            del %TARGET_LIB_DIR%\tipmt.lib
-   if exist %TARGET_LIB_DIR%\vm.lib               del %TARGET_LIB_DIR%\vm.lib
-   if exist %TARGET_LIB_DIR%\vmmt.lib             del %TARGET_LIB_DIR%\vmmt.lib
+   if exist %LIB_DIR%\codepage.lib         del %LIB_DIR%\codepage.lib
+   if exist %LIB_DIR%\common.lib           del %LIB_DIR%\common.lib
+   if exist %LIB_DIR%\ct.lib               del %LIB_DIR%\ct.lib
+   if exist %LIB_DIR%\ctmt.lib             del %LIB_DIR%\ctmt.lib
+   if exist %LIB_DIR%\dbfcdx.lib           del %LIB_DIR%\dbfcdx.lib
+   if exist %LIB_DIR%\dbfcdxmt.lib         del %LIB_DIR%\dbfcdxmt.lib
+   if exist %LIB_DIR%\dbfdbt.lib           del %LIB_DIR%\dbfdbt.lib
+   if exist %LIB_DIR%\dbfdbtmt.lib         del %LIB_DIR%\dbfdbtmt.lib
+   if exist %LIB_DIR%\dbffpt.lib           del %LIB_DIR%\dbffpt.lib
+   if exist %LIB_DIR%\dbffptmt.lib         del %LIB_DIR%\dbffptmt.lib
+   if exist %LIB_DIR%\dbfntx.lib           del %LIB_DIR%\dbfntx.lib
+   if exist %LIB_DIR%\dbfntxmt.lib         del %LIB_DIR%\dbfntxmt.lib
+   if exist %LIB_DIR%\debug.lib            del %LIB_DIR%\debug.lib
+   if exist %LIB_DIR%\dllmain.lib          del %LIB_DIR%\dllmain.lib
+   if exist %LIB_DIR%\harbour.lib          del %LIB_DIR%\harbour.lib
+   if exist %LIB_DIR%\hbodbc.lib           del %LIB_DIR%\hbodbc.lib
+   if exist %LIB_DIR%\hbodbcmt.lib         del %LIB_DIR%\hbodbcmt.lib
+   if exist %LIB_DIR%\hbsix.lib            del %LIB_DIR%\hbsix.lib
+   if exist %LIB_DIR%\hbsixmt.lib          del %LIB_DIR%\hbsixmt.lib
+   if exist %LIB_DIR%\hsx.lib              del %LIB_DIR%\hsx.lib
+   if exist %LIB_DIR%\hsxmt.lib            del %LIB_DIR%\hsxmt.lib
+   if exist %LIB_DIR%\gtnul.lib            del %LIB_DIR%\gtnul.lib
+   if exist %LIB_DIR%\gtstd.lib            del %LIB_DIR%\gtstd.lib
+   if exist %LIB_DIR%\gtwin.lib            del %LIB_DIR%\gtwin.lib
+   if exist %LIB_DIR%\gtcgi.lib            del %LIB_DIR%\gtcgi.lib
+   if exist %LIB_DIR%\gtpca.lib            del %LIB_DIR%\gtpca.lib
+   if exist %LIB_DIR%\gtwvt.lib            del %LIB_DIR%\gtwvt.lib
+   if exist %LIB_DIR%\lang.lib             del %LIB_DIR%\lang.lib
+   if exist %LIB_DIR%\macro.lib            del %LIB_DIR%\macro.lib
+   if exist %LIB_DIR%\macromt.lib          del %LIB_DIR%\macromt.lib
+   if exist %LIB_DIR%\nulsys.lib           del %LIB_DIR%\nulsys.lib
+   if exist %LIB_DIR%\optcon.lib           del %LIB_DIR%\optcon.lib
+   if exist %LIB_DIR%\optconmt.lib         del %LIB_DIR%\optconmt.lib
+   if exist %LIB_DIR%\optgui.lib           del %LIB_DIR%\optgui.lib
+   if exist %LIB_DIR%\optguimt.lib         del %LIB_DIR%\optguimt.lib
+   if exist %LIB_DIR%\pcrepos.lib          del %LIB_DIR%\pcrepos.lib
+   if exist %LIB_DIR%\pcreposmt.lib        del %LIB_DIR%\pcreposmt.lib
+   if exist %LIB_DIR%\pp.lib               del %LIB_DIR%\pp.lib
+   if exist %LIB_DIR%\ppmt.lib             del %LIB_DIR%\ppmt.lib
+   if exist %LIB_DIR%\rdd.lib              del %LIB_DIR%\rdd.lib
+   if exist %LIB_DIR%\rddmt.lib            del %LIB_DIR%\rddmt.lib
+   if exist %LIB_DIR%\rtl.lib              del %LIB_DIR%\rtl.lib
+   if exist %LIB_DIR%\rtlmt.lib            del %LIB_DIR%\rtlmt.lib
+   if exist %LIB_DIR%\tip.lib              del %LIB_DIR%\tip.lib
+   if exist %LIB_DIR%\tipmt.lib            del %LIB_DIR%\tipmt.lib
+   if exist %LIB_DIR%\vm.lib               del %LIB_DIR%\vm.lib
+   if exist %LIB_DIR%\vmmt.lib             del %LIB_DIR%\vmmt.lib
 
    REM Cleaning temp obj folder
 
-   if exist %TARGET_OBJ_DIR%\*.c                  del %TARGET_OBJ_DIR%\*.c
-   if exist %TARGET_OBJ_DIR%\*.h                  del %TARGET_OBJ_DIR%\*.h
-   if exist %TARGET_OBJ_DIR%\*.obj                del %TARGET_OBJ_DIR%\*.obj
-   if exist %TARGET_OBJ_DIR%\*.output             del %TARGET_OBJ_DIR%\*.output
+   if exist %OBJ_DIR%\*.c                  del %OBJ_DIR%\*.c
+   if exist %OBJ_DIR%\*.h                  del %OBJ_DIR%\*.h
+   if exist %OBJ_DIR%\*.obj                del %OBJ_DIR%\*.obj
+   if exist %OBJ_DIR%\*.output             del %OBJ_DIR%\*.output
 
-   if exist %TARGET_OBJ_DIR%\ct\*.c               del %TARGET_OBJ_DIR%\ct\*.c
-   if exist %TARGET_OBJ_DIR%\ct\*.obj             del %TARGET_OBJ_DIR%\ct\*.obj
+   if exist %OBJ_DIR%\ct\*.c               del %OBJ_DIR%\ct\*.c
+   if exist %OBJ_DIR%\ct\*.obj             del %OBJ_DIR%\ct\*.obj
 
-   if exist %TARGET_OBJ_DIR%\opt\*.c              del %TARGET_OBJ_DIR%\opt\*.c
-   if exist %TARGET_OBJ_DIR%\opt\*.obj            del %TARGET_OBJ_DIR%\opt\*.obj
+   if exist %OBJ_DIR%\opt\*.c              del %OBJ_DIR%\opt\*.c
+   if exist %OBJ_DIR%\opt\*.obj            del %OBJ_DIR%\opt\*.obj
 
-   if exist %TARGET_OBJ_DIR%\opt\console\*.c      del %TARGET_OBJ_DIR%\opt\console\*.c
-   if exist %TARGET_OBJ_DIR%\opt\console\*.obj    del %TARGET_OBJ_DIR%\opt\console\*.obj
+   if exist %OBJ_DIR%\opt\console\*.c      del %OBJ_DIR%\opt\console\*.c
+   if exist %OBJ_DIR%\opt\console\*.obj    del %OBJ_DIR%\opt\console\*.obj
 
-   if exist %TARGET_OBJ_DIR%\opt\gui\*.c          del %TARGET_OBJ_DIR%\opt\gui\*.c
-   if exist %TARGET_OBJ_DIR%\opt\gui\*.obj        del %TARGET_OBJ_DIR%\opt\gui\*.obj
+   if exist %OBJ_DIR%\opt\gui\*.c          del %OBJ_DIR%\opt\gui\*.c
+   if exist %OBJ_DIR%\opt\gui\*.obj        del %OBJ_DIR%\opt\gui\*.obj
 
-   if exist %TARGET_OBJ_DIR%\mt\*.c               del %TARGET_OBJ_DIR%\mt\*.c
-   if exist %TARGET_OBJ_DIR%\mt\*.h               del %TARGET_OBJ_DIR%\mt\*.h
-   if exist %TARGET_OBJ_DIR%\mt\*.obj             del %TARGET_OBJ_DIR%\mt\*.obj
+   if exist %OBJ_DIR%\mt\*.c               del %OBJ_DIR%\mt\*.c
+   if exist %OBJ_DIR%\mt\*.h               del %OBJ_DIR%\mt\*.h
+   if exist %OBJ_DIR%\mt\*.obj             del %OBJ_DIR%\mt\*.obj
 
-   if exist %TARGET_OBJ_DIR%\mt\opt\gui\*.c       del %TARGET_OBJ_DIR%\mt\opt\gui\*.c
-   if exist %TARGET_OBJ_DIR%\mt\opt\gui\*.obj     del %TARGET_OBJ_DIR%\mt\opt\gui\*.obj
+   if exist %OBJ_DIR%\mt\opt\gui\*.c       del %OBJ_DIR%\mt\opt\gui\*.c
+   if exist %OBJ_DIR%\mt\opt\gui\*.obj     del %OBJ_DIR%\mt\opt\gui\*.obj
 
-   if exist %TARGET_OBJ_DIR%\mt\opt\console\*.c   del %TARGET_OBJ_DIR%\mt\opt\console\*.c
-   if exist %TARGET_OBJ_DIR%\mt\opt\console\*.obj del %TARGET_OBJ_DIR%\mt\opt\console\*.obj
+   if exist %OBJ_DIR%\mt\opt\console\*.c   del %OBJ_DIR%\mt\opt\console\*.c
+   if exist %OBJ_DIR%\mt\opt\console\*.obj del %OBJ_DIR%\mt\opt\console\*.obj
 
-   if exist %TARGET_OBJ_DIR%\mt\opt\gui\*.c       del %TARGET_OBJ_DIR%\mt\opt\gui\*.c
-   if exist %TARGET_OBJ_DIR%\mt\opt\gui\*.obj     del %TARGET_OBJ_DIR%\mt\opt\gui\*.obj
+   if exist %OBJ_DIR%\mt\opt\gui\*.c       del %OBJ_DIR%\mt\opt\gui\*.c
+   if exist %OBJ_DIR%\mt\opt\gui\*.obj     del %OBJ_DIR%\mt\opt\gui\*.obj
 
-   if exist %TARGET_OBJ_DIR%\mt\ct\*.c            del %TARGET_OBJ_DIR%\mt\ct\*.c
-   if exist %TARGET_OBJ_DIR%\mt\ct\*.obj          del %TARGET_OBJ_DIR%\mt\ct\*.obj
+   if exist %OBJ_DIR%\mt\ct\*.c            del %OBJ_DIR%\mt\ct\*.c
+   if exist %OBJ_DIR%\mt\ct\*.obj          del %OBJ_DIR%\mt\ct\*.obj
 
    goto EXIT
 
@@ -263,9 +260,9 @@ SET HB_DIR=
 SET BISON_DIR=
 SET BISON_SIMPLE=
 
-SET TARGET_OBJ_DIR=
-SET TARGET_LIB_DIR=
-SET TARGET_BIN_DIR=
+SET OBJ_DIR=
+SET LIB_DIR=
+SET BIN_DIR=
 SET HB_MT=
 
 SET PATH=%_PATH%
