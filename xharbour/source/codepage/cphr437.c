9d63130ff53ca0a219fe89a62539a85fb23e77fe
Pavel Tsarenko <tpe2@mail.ru>
2006-10-10 17:47:46 +0000

    2006-10-09 22:00 UTC+0300 Pavel Tsarenko <tpe2@mail.ru>   * makefile.wc   * source/codepage/cphr437.c     ! fixed 2 typos in my previos commit

--- xharbour/source/codepage/cphr437.c
+++ xharbour/source/codepage/cphr437.c
@@ -1,5 +1,5 @@
 /*
- * $Id: cphr437.c,v 1.6 2006/04/06 19:22:02 paultucker Exp $
+ * $Id: cphr437.c,v 1.6 2006/10/09 19:23:44 ptsarenko Exp $
  */
 
 /*
@@ -86,7 +86,7 @@
 
 static HB_CODEPAGE s_codepage = { "HR437",
     CPID_437,UNITB_437,NUMBER_OF_CHARACTERS,
-    "ABC^]D\\EFGHIJKLMNOPQRS[TUVWXYZ@",
+    "ABC^]D\EFGHIJKLMNOPQRS[TUVWXYZ@",
     "abc~}d|efghijklmnopqrs{tuvwxyz`",
     IS_LATIN, ACCENTED_EQUAL, ACCENTED_INTERLEAVED, 0, 0, NULL, NULL, NULL, NULL, 0, NULL };
 
