2e51de1ddc2fd21bf692789f9ab54cf55224cc6a
Ron Pinkas <ron@ronpinkas.com>
2006-10-13 22:48:21 +0000

    2006-10-13 18:47 UTC-0500 Ron Pinkas <ron/at/xharbour.com>   * contrib/rdd_ads/rddads.h      * Changed ADS_REQUIRE_VERSION to 8 as we not yet use any new features specific to 8.1

--- xharbour/contrib/rdd_ads/rddads.h
+++ xharbour/contrib/rdd_ads/rddads.h
@@ -1,5 +1,5 @@
 /*
- * $Id: rddads.h,v 1.13 2006/10/12 19:44:20 ronpinkas Exp $
+ * $Id: rddads.h,v 1.14 2006/10/12 19:47:33 ronpinkas Exp $
  */
 
 /*
@@ -51,7 +51,7 @@
  */
 
 #ifndef ADS_REQUIRE_VERSION
-   #define ADS_REQUIRE_VERSION 81 // Must use 2 digits convention now!!!
+   #define ADS_REQUIRE_VERSION 8
 #endif
 
 #include "hbapirdd.h"
