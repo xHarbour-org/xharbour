5436124816507c859c05ff82920d1b2219ccf4af
marchuet <marchuet@unknown.com>
2008-07-30 16:35:30 +0000

    2008-07-30 13:33 UTC+0100 Miguel Angel Marchuet <miguelangel@marchuet.net>   * common.mak   * compile.mak   * contrib\hbmzip\common.mak   * makefile.bc   * makefile.dc   * makefile.gc   * makefile.vc   * makefile.wc   * mdir.bat     + Added mzip contrib library low size and hight speed than hbzip. many thank's to Mindaugas Kavaliauskas       and others contributors of harbour.
      * include\hbapi.h
      * source\rtl\strmatch.c
      * source\vm\itemapi.c
      * source\vm\extend.c
        * changed return value of hb_storclenAdopt, 0 when fails.
        + Added hb_strMatchFile( const char * pszString, const char * szPattern );
          that compare two strings using platform dependent rules for file matching
        + Added hb_strMatchCaseWildExact( const char *szString, const char *szPattern );
          that compare two strings using pattern with wildcard (?*) ignoring the case of the characters - pattern have to cover whole string
        + Added high level function HB_FILEMATCH. similar to WILDMATCH, but work with filenames.
    
      * include\hbdate.h
      * source\common\hbdate.c
        + Added hb_timeStrGet( const char * szTime, int * piHour, int * piMinutes, int * piSeconds, int * piMSec );
      synced with harbour as possible

--- xharbour/contrib/hbmzip/make_b32.bat
+++ xharbour/contrib/hbmzip/make_b32.bat
@@ -1,25 +1,19 @@
 @echo off
 rem
-rem $Id: make_b32.bat 8596 2008-06-04 02:03:48Z druzus $
+rem $Id: make_b32.bat,v 1.1 2008/07/30 10:15:56 marchuet Exp $
 rem
 
-if "%1" == "clean" goto CLEAN
-if "%1" == "CLEAN" goto CLEAN
-
-if not exist lib md lib
 if not exist obj md obj
 if not exist obj\b32 md obj\b32
-if not exist obj\b32\mt md obj\b32\mt
 :BUILD
 
-make -l EXE_OBJ_DIR=obj\b32\bin OBJ_DIR=obj\b32 -fmakefile.bc %1 %2 %3 > make_b32.log
+make -fmakefile.bc %1 %2 %3 > make_b32.log
+if "%1" == "clean" goto CLEAN
+if "%1" == "CLEAN" goto CLEAN
 if errorlevel 1 goto BUILD_ERR
-make -l OBJ_DIR=obj\b32\mt -DHB_THREAD_SUPPORT -DHB_MT=mt -fmakefile.bc %2 %3 >> make_b32.log
-if errorlevel 1 goto BUILD_ERR
-
 
 :BUILD_OK
-
+   copy ..\..\lib\b32\hbmzip.lib ..\..\lib > nul
    goto EXIT
 
 :BUILD_ERR
