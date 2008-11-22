10d9693188223e12b8a51d80b4a02bf48c1a1dd4
Luiz Rafael Culik <culik@sl.conex.net>
2008-11-22 09:48:46 +0000

    2008-11-22 07:50 UTC-0300 Luiz Rafael Culik Guimaraes <luiz at xharbour.com.br>   * contrib/hbzlib/include/zipautobuffer.h     ! minor fix to compile under msvc

--- xharbour/contrib/hbzlib/include/zipautobuffer.h
+++ xharbour/contrib/hbzlib/include/zipautobuffer.h
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 // $Workfile: ZipAutoBuffer.h $
 // $Archive: /ZipArchive/ZipAutoBuffer.h $
-// $Date: 2008/05/01 10:49:39 $ $Author: andijahja $
+// $Date: 2008/11/19 15:04:30 $ $Author: lculik $
 ////////////////////////////////////////////////////////////////////////////////
 // This source file is part of the ZipArchive library source distribution and
 // is Copyright 2000-2003 by Tadeusz Dracz (http://www.artpol-software.com/)
@@ -34,7 +34,7 @@
 class ZIP_API CZipAutoBuffer
 {
 public:
-        operator char*() const
+	operator char*()
 	{
 		return m_pBuffer;
 	}
