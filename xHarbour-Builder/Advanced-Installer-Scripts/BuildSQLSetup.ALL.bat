d38f424a056d73576f02599663089f3b43d6e6ac
Patrick Mast <>
2011-02-24 13:09:40 +0000

    no message

--- xHarbour-Builder/Advanced-Installer-Scripts/BuildSQLSetup.ALL.bat
+++ xHarbour-Builder/Advanced-Installer-Scripts/BuildSQLSetup.ALL.bat
@@ -3,11 +3,11 @@
    @call W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\CopyFiles-SQLRDD.bat XHB-RELEASE
    W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\xHB-SQLRDD-Setup.exe W XHB-RELEASE
 
-REM @call W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\CopyFiles-SQLRDD.bat XHB-DEMO
-REM W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\xHB-SQLRDD-Setup.exe W XHB-DEMO
+   @call W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\CopyFiles-SQLRDD.bat XHB-DEMO
+   W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\xHB-SQLRDD-Setup.exe W XHB-DEMO
 
-    @call W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\CopyFiles-SQLRDD.bat BCC-RELEASE
-    W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\xHB-SQLRDD-Setup.exe W BCC-RELEASE
+   @call W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\CopyFiles-SQLRDD.bat BCC-RELEASE
+   W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\xHB-SQLRDD-Setup.exe W BCC-RELEASE
 
 REM @call W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\CopyFiles-SQLRDD.bat BCC-DEMO
 REM W:\xHarbour.com\xHarbour-Builder\Advanced-Installer-Scripts\xHB-SQLRDD-Setup.exe W BCC-DEMO
