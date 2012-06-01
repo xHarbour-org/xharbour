631c759c46589ec4d2667e38e51eb56d49bc517a
Patrick Mast <>
2012-06-01 08:14:45 +0000

    no message

--- xHarbour-SQLRDD/bc5/Build_All.bat
+++ xHarbour-SQLRDD/bc5/Build_All.bat
@@ -1,22 +1,14 @@
-    MD %XHB_BUILD_DRIVE%:\xharbour\xbp\obj-bc5\sql.lib
-    MD %XHB_BUILD_DRIVE%:\xharbour\xbp\obj-demo-bc5\sql-demo.lib
-    MD %XHB_BUILD_DRIVE%:\xharbour\xbp\obj-demo-bc5\mysql-demo.lib
 
-    IF "%PROG_PATH%"=="" SET PROG_PATH=%ProgramFiles%
-    IF "%XHB_BUILD_DRIVE%" == "" SET XHB_BUILD_DRIVE=C
-    IF "%BCCDIR%" == "" SET BCCDIR=C:\BCC55
+    IF "%XHB_BCC%" == "" SET XHB_BCC=C:\xHarbour
+    IF "%BCCDIR%" == "" SET BCCDIR=C:\Borland\BCC55
 
-    SET PATH=%XHB_BUILD_DRIVE%:\xHarbour\bin;"C:\Program Files\GnuWin32\bin"
+    MD C:\xharbour\xbp\obj-bc55\sql.lib
+    MD C:\xharbour\xbp\obj-demo-bc55\sql-demo.lib
+
+    SET PATH=C:\xHarbour\bin
 
     SET INCLUDE=
     SET LIB=
 
-    REM BISON > NUL:
-
-    \xHB\Bin\xBuild.exe sql.lib.xbp                     -NoXbp %1
-    \xHB\Bin\xBuild.exe sql-demo.lib.xbp                -NoXbp %1
-    \xHB\Bin\xBuild.exe sqlmt.lib.xbp                   -NoXbp %1
-
-    \xHB\Bin\xBuild.exe mysql.lib.xbp                   -NoXbp %1
-    \xHB\Bin\xBuild.exe mysql-demo.lib.xbp              -NoXbp %1
-    \xHB\Bin\xBuild.exe mysqlmt.lib.xbp                 -NoXbp %1
+    \xHB\Bin\xBuild.exe sql.lib.xbp      -NoXbp %1
+rem    \xHB\Bin\xBuild.exe sqlmt.lib.xbp    -NoXbp %1
