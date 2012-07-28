2cd85ca9ae6478c21a21b9e2f45437f6ac27eb25
Patrick Mast <>
2012-07-28 20:29:23 +0000

    no message

--- xHarbour-SQLRDD/bc5/Demo/Build_bcc.bat
+++
@@ -1,8 +0,0 @@
-SET XHB_BCC=C:\xHarbour
-SET BCCDIR=C:\Borland\BCC55
-
-RD \xbp\bc5\demo\sql.lib /S /Q 
-
-    \xHB\Bin\xBuild.exe sql.lib.xbp   -NoXbp %1
-
-REN \xhb\lib\bcc\sql.lib SQLBDEMOCC55.lib
