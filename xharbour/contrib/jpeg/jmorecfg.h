c9cb213fae1b7a11f07061059ef8e5de8aac1a91
Luiz Rafael Culik <culik@sl.conex.net>
2012-08-17 13:15:22 +0000

    2012-08-17 10:20  UTC-0300 Luiz Rafael Culik <luiz at xharbour.com.br>   * contrib/jpeg/jmorecfg.h     ! avoid redefinition of INT16 and UINT'6 with HB_DEFS_H_

--- xharbour/contrib/jpeg/jmorecfg.h
+++ xharbour/contrib/jpeg/jmorecfg.h
@@ -145,19 +145,19 @@ typedef short UINT8;
 #endif /* HAVE_UNSIGNED_CHAR */
 
 /* UINT16 must hold at least the values 0..65535. */
-
+#ifndef HB_DEFS_H_
 #ifdef HAVE_UNSIGNED_SHORT
 typedef unsigned short UINT16;
 #else /* not HAVE_UNSIGNED_SHORT */
 typedef unsigned int UINT16;
 #endif /* HAVE_UNSIGNED_SHORT */
-
+#endif
 /* INT16 must hold at least the values -32768..32767. */
-
+#ifndef HB_DEFS_H_
 #ifndef XMD_H			/* X11/xmd.h correctly defines INT16 */
 typedef short INT16;
 #endif
-
+#endif
 /* INT32 must hold at least signed 32-bit values. */
 
 #ifndef HB_DEFS_H_
