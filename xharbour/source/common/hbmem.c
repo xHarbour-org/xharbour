5263b36d7976c76cb84a0752941230ef655b0300
Ron Pinkas <ron@ronpinkas.com>
2012-09-18 16:19:56 +0000

    2012-09-18 11:49 UTC-0430 Ron Pinkas <ron.pinkas/at/xharbour.com>   * source/common/hbmem.c     * Minor correction to declaration of dv_memcpy()

--- xharbour/source/common/hbmem.c
+++ xharbour/source/common/hbmem.c
@@ -5,28 +5,28 @@
  ** File:     memcpy.c
  **
  ** Copyright (C) 1999-2010 Daniel Vik
- ** 
+ **
  ** This software is provided 'as-is', without any express or implied
  ** warranty. In no event will the authors be held liable for any
  ** damages arising from the use of this software.
  ** Permission is granted to anyone to use this software for any
  ** purpose, including commercial applications, and to alter it and
  ** redistribute it freely, subject to the following restrictions:
- ** 
+ **
  ** 1. The origin of this software must not be misrepresented; you
  **    must not claim that you wrote the original software. If you
  **    use this software in a product, an acknowledgment in the
  **    use this software in a product, an acknowledgment in the
  **    product documentation would be appreciated but is not
  **    required.
- ** 
+ **
  ** 2. Altered source versions must be plainly marked as such, and
  **    must not be misrepresented as being the original software.
  **
  ** 3. This notice may not be removed or altered from any source
  **    distribution.
- ** 
- ** 
+ **
+ **
  ** Description: Implementation of the standard library function memcpy.
  **             This implementation of memcpy() is ANSI-C89 compatible.
  **
@@ -43,7 +43,7 @@
  **                             Using this option, disables the
  **                             PRE_INC_PTRS option.
  **
- **           MEMCPY_64BIT    - Compiles memcpy for 64 bit 
+ **           MEMCPY_64BIT    - Compiles memcpy for 64 bit
  **                             architectures
  **
  **
@@ -332,10 +332,8 @@ typedef UInt32              UIntN;
 */
 
 HB_EXTERN_BEGIN  /* Added for xHarbour */
-extern HB_EXPORT void *dv_memcpy(void *dest, const void *src, size_t count);
-HB_EXTERN_END    /* Added for xHarbour */
 
-void *dv_memcpy(void *dest, const void *src, size_t count)
+HB_EXPORT void *dv_memcpy(void *dest, const void *src, size_t count)
 {
     UInt8* dst8 = (UInt8*)dest;
     UInt8* src8 = (UInt8*)src;
@@ -368,6 +366,8 @@ void *dv_memcpy(void *dest, const void *src, size_t count)
 
     return NULL; /* pseudo return to avoid no-return-value warning */
 }
+
+HB_EXTERN_END    /* Added for xHarbour */
 
 /*
 1. Introduction
