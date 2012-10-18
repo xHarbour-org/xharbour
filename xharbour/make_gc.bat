578a1e4319a635e57b078bba4318b16d906d0366
Andi Jahja <andi.jahja@yahoo.co.id>
2012-10-18 20:38:51 +0000

    2012-10-19 03:36 UTC+0700 Andi Jahja <xharbour/AT/telkom/net/id>   * make_gc.bat   * winmake/makefile.gc     ! corrected syntax error: if -> ifeq     + append 'd' in library and executable file names when HB_DEBUG=1

--- xharbour/make_gc.bat
+++ xharbour/make_gc.bat
@@ -30,8 +30,8 @@ rem The followings should never change
 rem Do not hard-code in makefile because there are needed for clean build
 rem ============================================================================
 SET DIR_SEP=/
-SET OBJEXT=%HB_ARCH%.o
-SET LIBEXT=%HB_ARCH%.a
+SET OBJEXT=%HB_ARCH%%HB_DEBUG%.o
+SET LIBEXT=%HB_ARCH%%HB_DEBUG%.a
 SET LIBPREFIX=lib
 
 if "%1"=="/?"      goto SYNTAX
