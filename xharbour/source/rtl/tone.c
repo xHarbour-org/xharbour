9b5199c5e53080f94ed233aa8b0cc1ae73ec2d76
Andi Jahja <andi.jahja@yahoo.co.id>
2012-12-12 11:54:56 +0000

    2012-12-12 18:51 UTC+0700 Andi Jahja <xharbour/AT/telkom/net/id>   * source/rtl/tone.c     + HB_TONE(), tone functionality for Windows OS via MMSYSTEM
      + tests/testtone.prg
        ! test program for HB_TONE()

--- xharbour/source/rtl/tone.c
+++ xharbour/source/rtl/tone.c
@@ -58,3 +58,130 @@ HB_FUNC( TONE )
    if( ISNUM( 1 ) )
       hb_gtTone( hb_parnd( 1 ), ( ISNUM( 2 ) ? hb_parnd( 2 ) : 1.0 ) );
 }
+
+#if defined( HB_OS_WIN )
+
+#include "hbapi.h"
+#include <windows.h>
+#include <mmsystem.h>
+#include <math.h>
+
+HB_EXTERN_BEGIN
+void _winTone( UINT uFrequency, double dwDuration, BOOL bSmoothing, UINT uVolume );
+HB_EXTERN_END
+#define SAMPLING_RATE 44100
+
+HB_FUNC( HB_TONE )
+{
+   if ISNUM( 1 )
+   {
+      _winTone( hb_parni( 1 ),
+                  ( ISNUM( 2 ) ? hb_parnd( 2 ) : 1.0 ) * 2100,
+                  ISLOG( 3 ) ? hb_parl( 3 ) : TRUE,
+                  ISNUM( 4 ) ? hb_parni( 4 ) : 127 );
+   }
+}
+
+/* Keep it here for access from C programs */
+void _winTone( UINT uFrequency, double dwDuration, BOOL bSmoothing, UINT uVolume )
+{
+   WAVEFORMATEX wfx;
+   HWAVEOUT hWaveOut = NULL;
+
+   wfx.wFormatTag = WAVE_FORMAT_PCM;
+   wfx.nChannels = 1;
+   wfx.nSamplesPerSec = SAMPLING_RATE;
+   wfx.nAvgBytesPerSec = SAMPLING_RATE;
+   wfx.nBlockAlign = 1;
+   wfx.wBitsPerSample = 8;
+   wfx.cbSize = 0;
+
+   /*
+   WAVE_MAPPER is the most commonly used device
+   CallBack is unnecessary since we only play simple tone
+   */
+   /* if( waveOutOpen( &hWaveOut, WAVE_MAPPER, &wfx, NULL, NULL, CALLBACK_NULL ) == MMSYSERR_NOERROR )
+    */
+   if( waveOutOpen( &hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL ) == MMSYSERR_NOERROR )
+   {
+      char amp = uVolume;
+      int i;
+      unsigned char* buffer = (unsigned char*) hb_xgrab( (ULONG) dwDuration );
+      double dKoef = uFrequency * 2 * 3.1416 / SAMPLING_RATE;
+      WAVEHDR wh;
+
+      if ( buffer )
+      {
+         wh.lpData = (LPSTR) buffer;
+         wh.dwBufferLength = (DWORD) dwDuration;
+         wh.dwFlags = WHDR_BEGINLOOP;
+         wh.dwLoops = 1;
+
+         if( waveOutPrepareHeader( hWaveOut, &wh, sizeof( wh ) ) == MMSYSERR_NOERROR )
+         {
+            wh.dwFlags = WHDR_BEGINLOOP | WHDR_ENDLOOP | WHDR_PREPARED;
+            wh.dwLoops = 1;
+
+            if ( bSmoothing )
+            {
+               /*
+                * Manipulating data to smooth sound from clicks at the start
+                * and end (particularly noted when we call TONE() many times
+                * in a row). This is a simulation of increasing volume gradually
+                * before it reaches the peak, and decreasing volume gradually
+                * before it reaches the end.
+                */
+               for( i = 0; i < dwDuration; i++ )
+               {
+                  if ( i < amp )
+                  {
+                     buffer[ i ] = (unsigned char) ( cos( i * dKoef ) * i + 127 );
+                  }
+                  else if ( dwDuration - i <= amp - 1  )
+                  {
+                     //amp = max( 0, --amp );
+                     if( --amp < 0 )
+		        amp = 0;
+                     buffer[ i ] = (unsigned char) ( cos( i * dKoef ) * amp + 127 );
+                  }
+                  else
+                  {
+                     buffer[ i ] = (unsigned char) ( cos( i * dKoef ) * amp + 127 );
+                  }
+               }
+            }
+            else
+            {
+               /*
+                * Raw sound, may cause annoying clicks when some tones are played
+                * in a row.
+                */
+               for( i = 0; i < (int) dwDuration; i++ )
+               {
+                  buffer[ i ] = (unsigned char) ( cos( i * dKoef ) * amp + 127 );
+               }
+            }
+
+            /*
+             * Play the sound here
+             */
+            if ( waveOutWrite( hWaveOut, &wh, sizeof(WAVEHDR) ) == MMSYSERR_NOERROR )
+            {
+               /*
+                * Wait until the sound is finished
+                */
+               while (!(wh.dwFlags & WHDR_DONE))
+               {
+                  Sleep( 1 );
+               }
+
+               waveOutUnprepareHeader(hWaveOut, &wh, sizeof(WAVEHDR));
+            }
+         }
+
+         hb_xfree( buffer );
+      }
+   }
+}
+
+#endif
