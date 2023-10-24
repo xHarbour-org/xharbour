8a56672886d01dddfa1118574af9fd06ad0987b1
ronpinkas <ron@ronpinkas.com>
2023-10-23 20:33:35 -0700

    2023-10-23 22:34 UTC-0500 Ron Pinkas <ronpinkas/AT/gmail/com>   * make_drw.sh     * Improved to use dynamicly extract from brew the path for zlib and and openssl   * make_macosx.sh     * Modified to source ./make_drw.sh

--- make_drw.sh
+++ make_drw.sh
@@ -30,14 +30,13 @@ fi
 export C_USR="-Weverything -Wno-cast-qual -Wno-format-nonliteral -Wno-declaration-after-statement -Wno-empty-translation-unit -Wno-newline-eof -Wno-date-time -Wno-double-promotion -Wno-shorten-64-to-32 -Wno-shadow -Wno-implicit-int-conversion -Wno-sign-conversion -Wno-poison-system-directories -Wno-padded -Wno-deprecated-declarations -Wno-cast-align -Wno-undef -Wno-unused-macros -Wno-missing-prototypes -Wno-invalid-source-encoding"
 export L_USR=""
 
-export C_USR="$C_USR -I/usr/local/opt/zlib/include"
-export L_USR="$L_USR -L/usr/local/opt/zlib/lib"
+LIB_NAMES=("zlib" "openssl" "X11")
+LIB_PREFIXES=($(brew --prefix zlib) $(brew --prefix openssl) "/opt/X11")
 
-export C_USR="$C_USR -I/usr/local/opt/openssl/include"
-export L_USR="$L_USR -L/usr/local/opt/openssl/lib"
-
-export C_USR="$C_USR -I/opt/X11/include"
-export L_USR="$L_USR -L/opt/X11/lib"
+for index in ${!LIB_NAMES[@]}; do
+  export C_USR="$C_USR -I${LIB_PREFIXES[$index]}/include"
+  export L_USR="$L_USR -L${LIB_PREFIXES[$index]}/lib"
+done
 
 #if you do not want DEBUG build, comment the following line
 export C_USR="$C_USR -g -O0 -fsanitize=address"
--- make_macosx.sh
+++ make_macosx.sh
@@ -1,18 +1,2 @@
 #!/bin/sh
-#
-# $Id$
-#
-
-# ---------------------------------------------------------------
-# Template to initialize the environment before starting
-# the GNU make system, so xHarbour can be built in MacOS X
-#
-# For further information about the GNU make system please
-# check doc/gmake.txt
-#
-# ---------------------------------------------------------------
-
-export HB_WITHOUT_GTSLN=yes
-#export HB_WITHOUT_X11=yes
-
-. `dirname $0`/make_gnu.sh $*
+. `dirname $0`/make_drw.sh $*
