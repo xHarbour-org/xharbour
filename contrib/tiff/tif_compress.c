2b2f6251e5988d3c2b556e9abe9b5b85ad20049f
Enrico Maria Giordano <e.m.giordano@emagsoftware.it>
2023-12-16 19:11:41 +0100

    Update tif_compress.c

--- contrib/tiff/tif_compress.c
+++ contrib/tiff/tif_compress.c
@@ -233,7 +233,7 @@ TIFFUnRegisterCODEC(TIFFCodec* c)
 	codec_t* cd;
 	codec_t** pcd;
 
-	for (pcd = &registeredCODECS; (cd = *pcd); pcd = &cd->next)
+	for (pcd = &registeredCODECS; (cd = *pcd) != 0; pcd = &cd->next)
 		if (cd->info == c) {
 			*pcd = cd->next;
 			_TIFFfree(cd);
