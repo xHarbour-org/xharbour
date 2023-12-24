9cb6e50f5b90bef1036e3abe65937ea55cd3c6ee
Enrico Maria Giordano <e.m.giordano@emagsoftware.it>
2023-12-24 10:51:50 +0100

    Update hbencode.c

--- contrib/unicode/hbencode.c
+++ contrib/unicode/hbencode.c
@@ -152,7 +152,7 @@
 static char basis_64[]     = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 static int  crc_val;
 static long crc_anz;
-static int  crc_tab[ 256 ] =
+static unsigned int  crc_tab[ 256 ] =
 {
    0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
    0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, 0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
