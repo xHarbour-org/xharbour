7a0f8d909db7ce80f0fc8afd35d0ef7e9ed4551a
Gianfranco Costamagna <locutusofborg@debian.org>
2025-08-18 10:36:23 +0200

    Cherry-pick harbour commit https://github.com/harbour/core/issues/388
     * include/hbapifs.h * src/rdd/wacore.c * src/rtl/filebuf.c
     + Fixed locking mechanism for SMB related networks and Linux kernel >= 5.5
     - Removed old HB_USE_BSDLOCKS implementation, now broken on Linux too Closes:
     https://github.com/harbour/core/issues/388 issue: #338
    
    PR: https://github.com/harbour/core/pull/389

--- include/hbapifs.h
+++ include/hbapifs.h
@@ -231,13 +231,6 @@ extern HB_EXPORT BOOL       hb_fsCloseProcess( HB_FHANDLE fhProc, BOOL bGentle )
 #  define HB_USE_SHARELOCKS
 #  define HB_SHARELOCK_POS          0x7fffffffUL
 #  define HB_SHARELOCK_SIZE         0x1UL
-#  if defined( HB_USE_BSDLOCKS_OFF )
-#     undef HB_USE_BSDLOCKS
-#  elif ( defined( HB_OS_LINUX ) /*|| defined( HB_OS_BSD )*/ ) && \
-        !defined( __WATCOMC__ ) && !defined( HB_USE_BSDLOCKS )
-      /* At least FreeBSD 6.2 and Mac OS X deadlock using BSDLOCKS so I disabled them -- Ph. */
-#     define HB_USE_BSDLOCKS
-#  endif
 #endif
 
 #define HB_MAX_DRIVE_LENGTH   10
